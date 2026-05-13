ed9b6510de0acdeee3d8037735feb1228720f1e3
urutoraktiti-boop <urutoraktiti@gmail.com>
2026-05-13 15:12:17 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -5,7 +5,7 @@
 //   2. index.html の CURRENT_VERSION（フォールバック値）
 //   3. version.json の version
 // ============================================================
-const CACHE_VERSION = 'kotsusaku-v65';
+const CACHE_VERSION = 'kotsusaku-v71';
 const CACHE_FILES = [
   './',
   './index.html',
